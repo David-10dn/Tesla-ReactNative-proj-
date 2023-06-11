f1871061e8b97c02289d9a1c9a15b9107e0c5c81
david-10dn <davnev0423@skola.goteborg.se>
2023-06-11 19:00:11 +0200

    adding Header

--- App.js
+++ App.js
@@ -1,22 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, View } from 'react-native';
 import React from 'react';
-import CarItem from './components/CarItem';
 import CarList from './components/CarList';
+import Header from './components/Header';
 
 export default function App() {
   return (
     <View style={styles.container}>
 
+      <Header />
+
       {/* its better to import all cars from CarList */}
       <CarList />
 
-      {/* <CarItem name={"Model X"}
-        tagline={"Order Online for "}
-        taglineCTA={"Touchless Delivery"}
-        image={require('./assets/images/ModelX.jpeg')}
-      /> */}
-
+      
       <StatusBar style="auto" />
     </View>
   );
